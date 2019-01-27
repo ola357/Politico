3464da39eed688f7a9de55339b51b0c806bd22f8
ola357 <alliolaoluwa@gmail.com>
2019-01-27 09:08:29 +0100

    ch-(Testing endpoints) updated the validation logic

--- middleware/validateParty.js
+++ middleware/validateParty.js
@@ -2,8 +2,9 @@ const Joi = require('joi');
 
 function validateParty(party) {
   const schema = {
-    name: Joi.string().max(5).required(),
-    hqAddress: Joi.string(),
+    name: Joi.string().min(2).max(5).required(),
+    hqAddress: Joi.string().required(),
+    logoUrl: Joi.string().required(),
   };
   return Joi.validate(party, schema);
 }
