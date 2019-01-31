2d85f9896e1bef4599223e28070e07972f33c229
ola357 <alliolaoluwa@gmail.com>
2019-01-31 12:37:26 +0100

    [chore #163619571] Setup test framework mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "rules": {
+    "linebreak-style": "off",
+    "no-undef": "off",
+    "arrow-body-style": "off",
+    "quotes": "off"
+}
 }
