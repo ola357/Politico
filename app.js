25d8ede47fc3426d857c429c90158ff6a1b80357
ola357 <alliolaoluwa@gmail.com>
2019-01-31 14:07:28 +0100

    [chore #163500926] Wrote unit tests for api endpoints

---
+++ app.js
@@ -0,0 +1,26 @@
+// const express = require('express');
+
+
+// const app = express();
+
+// app.use(express.json());
+
+// const parties = [
+//   {
+//     id: 1, name: 'APC', hqAddress: 'Lagos', logoUrl: 'a/b/c',
+//   },
+//   {
+//     id: 2, name: 'PDP', hqAddress: 'Abuja', logoUrl: 'd/e/f',
+//   },
+// ];
+// app.get('/api/v1/parties', (req, res) => {
+//   // eslint-disable-next-line no-console
+//   console.log(res);
+//   res.send({ status: 200, data: parties });
+// });
+
+
+// const port = process.env.PORT || 3000;
+// // eslint-disable-next-line no-console
+// const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+// module.exports = server;
