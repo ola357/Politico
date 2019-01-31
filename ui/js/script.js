c1322f2888ba9fc4dcf3eaf2df5262a8d41e3f95
ola357 <alliolaoluwa@gmail.com>
2019-01-31 01:35:17 +0100

    renewed my branch with modified changes and ui

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
   $('#menu-btn').click(changeNav);
-
+  $('#links').toggle();
 
   function changeNav() {
-    $('#links').toggle(2000);
+    $('#links').toggle(800);
     $('#menu-btn').toggleClass('turn')
   }
 
